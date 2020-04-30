fe01b8397271925683b0a8a4cd4f9763b5634a06
Daan van Berkel <info@daanvanberkel.nl>
2020-04-30 11:16:56 +0200

    Circuit op juiste manier weergeven

--- src/main/java/nl/daanvanberkel/LogischCircuit/Models/Circuit.java
+++ src/main/java/nl/daanvanberkel/LogischCircuit/Models/Circuit.java
@@ -5,18 +5,24 @@ import java.util.Collections;
 import java.util.List;
 
 public class Circuit implements OutputHandler {
-    private final ArrayList<InputNode> inputNodes = new ArrayList<InputNode>();
+    private final ArrayList<InputNode> inputNodes = new ArrayList<>();
+    private final ArrayList<OutputNode> outputNodes = new ArrayList<>();
 
     public void addInputNode(InputNode node) {
         inputNodes.add(node);
     }
 
     public void addOutputNode(OutputNode node) {
+        outputNodes.add(node);
         node.setOutputHandler(this);
     }
 
     public List<InputNode> getInputNodes() {
         return Collections.unmodifiableList(inputNodes);
+    }
+
+    public List<OutputNode> getOutputNodes() {
+        return Collections.unmodifiableList(outputNodes);
     }
 
     public void start() {
--- src/main/java/nl/daanvanberkel/LogischCircuit/Views/CircuitView.java
+++ src/main/java/nl/daanvanberkel/LogischCircuit/Views/CircuitView.java
@@ -2,24 +2,32 @@ package nl.daanvanberkel.LogischCircuit.Views;
 
 import nl.daanvanberkel.LogischCircuit.Models.Circuit;
 import nl.daanvanberkel.LogischCircuit.Models.InputNode;
+import nl.daanvanberkel.LogischCircuit.Models.Node;
+import nl.daanvanberkel.LogischCircuit.Models.OutputNode;
 import org.graphstream.graph.Graph;
-import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.ui.swingViewer.ViewPanel;
 import org.graphstream.ui.view.Viewer;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class CircuitView extends JPanel {
+    private static final int WINDOW_WIDTH = 1000;
+    private static final int WINDOW_HEIGHT = 500;
+
     private Circuit circuit;
-    private NodeView[] nodeViews;
     private JFrame frame;
+    private HashMap<Integer, ArrayList<Node>> levels;
 
     public CircuitView(Circuit circuit) {
         this.circuit = circuit;
 
-        setPreferredSize(new Dimension(1000, 1000));
+        setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
         setLayout(new GridLayout());
 
         frame = new JFrame();
@@ -28,19 +36,29 @@ public class CircuitView extends JPanel {
     }
 
     public void drawCircuit() {
-        Graph graph = new SingleGraph("circuit");
+        convertCircuitToLevels(circuit);
 
-        int i = 1;
-        for (InputNode node : circuit.getInputNodes()) {
-            Node gNode = graph.addNode(node.getName());
-            gNode.addAttribute("node", node);
-            gNode.setAttribute("xy", 1, i);
-            gNode.addAttribute("ui.label", node.getName());
-            addOutputNodes(node, graph, 2);
-            i++;
+        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+        Graph graph = new SingleGraph("circuit");
+        graph.addAttribute("ui.stylesheet", "node { shape: freeplane; fill-color: white; stroke-mode: plain; size-mode: fit; } edge { shape: freeplane; } edge:clicked { fill-color: #0f0; }");
+
+        for(Map.Entry<Integer, ArrayList<Node>> level : levels.entrySet()) {
+            int x = level.getKey() * (WINDOW_WIDTH / levels.size());
+            int y = 0;
+
+            for(Node node : level.getValue()) {
+                org.graphstream.graph.Node gNode = graph.addNode(node.getName());
+                gNode.addAttribute("xy", x, y);
+                gNode.addAttribute("ui.label", node.getName());
+
+                y += (WINDOW_HEIGHT / level.getValue().size());
+            }
         }
 
-        Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
+        addEdges(graph);
+
+        Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
+        viewer.disableAutoLayout();
         ViewPanel view = viewer.addDefaultView(false);
         add(view);
 
@@ -50,27 +68,79 @@ public class CircuitView extends JPanel {
         frame.setVisible(true);
     }
 
-    private void addOutputNodes(nl.daanvanberkel.LogischCircuit.Models.Node node, Graph graph, int x) {
-        int i = 1;
-        for(nl.daanvanberkel.LogischCircuit.Models.Node child : node.getOutputNodes()) {
-            System.out.println("Adding node " + child.getName() + ", x: " + x + ", y: " + i);
-
-            if (graph.getNode(child.getName()) == null) {
-                Node gNode = graph.addNode(child.getName());
-                gNode.addAttribute("node", child);
-                gNode.setAttribute("xy", x, i);
-                gNode.addAttribute("ui.label", child.getName());
-            } else {
-                System.out.println("Node " + child.getName() + " already exists");
+    private void addEdges(Graph graph) {
+        for(Map.Entry<Integer, ArrayList<Node>> level : levels.entrySet()) {
+            for(Node node : level.getValue()) {
+                for(Node child : node.getOutputNodes()) {
+                    if (graph.getEdge(node.getName() + child.getName()) == null) {
+                        graph.addEdge(node.getName() + child.getName(), node.getName(), child.getName());
+                    }
+                }
             }
-
-            if (graph.getEdge(node.getName() + child.getName()) == null) {
-                graph.addEdge(node.getName() + child.getName(), node.getName(), child.getName());
-            }
-
-            addOutputNodes(child, graph, x + 1);
-
-            i++;
         }
+    }
+
+    private void convertCircuitToLevels(Circuit circuit) {
+        levels = new HashMap<>();
+
+        levels.put(1, new ArrayList<>());
+
+        for(InputNode node : circuit.getInputNodes()) {
+            levels.get(1).add(node);
+
+            addCircuitLevel(node, 2);
+        }
+
+        Integer highestLevel = levels.size();
+
+        // Add all output nodes to the highest level
+        for(OutputNode node : circuit.getOutputNodes()) {
+            Integer currentLevel = getNodeLevel(node);
+
+            if (currentLevel != null && currentLevel < highestLevel) {
+                levels.get(currentLevel).remove(node);
+                levels.get(highestLevel).add(node);
+            }
+        }
+    }
+
+    private void addCircuitLevel(Node node, int level) {
+        if (node.getOutputNodes().size() < 1) {
+            return;
+        }
+
+        if (levels.get(level) == null) {
+            levels.put(level, new ArrayList<>());
+        }
+        
+        for(Node n : node.getOutputNodes()) {
+            // Check if node is already in a level
+            Integer nodeLevel = getNodeLevel(n);
+            if (nodeLevel != null) {
+                // If node is already in the list, but at a lower level -> move the node up to the right level
+                if (nodeLevel < level) {
+                    levels.get(nodeLevel).remove(n);
+                    levels.get(level).add(n);
+                }
+            } else {
+                levels.get(level).add(n);
+            }
+
+            addCircuitLevel(n, level + 1);
+        }
+    }
+
+    private Integer getNodeLevel(Node node) {
+        Iterator<Integer> iterator = levels.keySet().iterator();
+
+        while(iterator.hasNext()) {
+            Integer level = iterator.next();
+
+            if (levels.get(level).contains(node)) {
+                return level;
+            }
+        }
+
+        return null;
     }
 }
